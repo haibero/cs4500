44317c1fc4e7c4234d82ed633e76451964da5bc9
Barry Yung <byung@Barrys-MacBook-Pro.local>
2020-01-30 17:36:44 -0500

    array done

--- hw2/p1/array.h
+++ hw2/p1/array.h
@@ -1,159 +1,373 @@
 #pragma once
 #include <stdlib.h>
-#include "Object.h"
 
 class Array : public Object {
   public:
   	size_t len_;
   	size_t count_;
-  	
+    size_t lastElemIndex_;
+
 
     // Constructor
   	Array(size_t len) {
   		len_ = len;
   		count_ = 0;
+      lastElemIndex_ = 0;
   	}
 
   	// Destructor
   	virtual ~Array() {}
 
   	// inserts the given object into the array and returns the array
-  	virtual void insert(void* obj, size_t index) {
-
-  	}
+  	// virtual void insert(void* obj, size_t index) {}
 
   	// remove the item at the given index
-  	virtual void* remove(size_t index) {
-
-  	}
+  	//virtual void* remove(size_t index) {
+    //}
 
   	// returns the length of the array
   	virtual size_t length() {
   		return len_;
   	}
-    
+    //
   	// returns the number of elements currently in the array (if implementing as resizeable arrayList, count of items may differ from total length)
   	virtual size_t count() {
   		return count_;
   	}
 
-  	// gets the object from data stored at index
-  	virtual void* get(size_t index){
+  	// // gets the object from data stored at index
+  	// virtual void* get(size_t index){
+  	// }
 
-  	}
 
-  	virtual void push_back(void* obj){
-  		
-  	}
+    //Increases the capacity of the array by double
+    virtual void grow(){
 
+    }
+
+    //Finds the index of the last element in the array
+    virtual size_t getLastIndex() {
+    }
 };
+
+class ObjArray : public Array {
+public:
+  Object** array_;
+
+  // Constructor
+  ObjArray(size_t len) : Array(len) {
+    array_ = new Object* [len];
+  }
+
+  void insert(Object* obj, size_t index) {
+      if(index > len_ - 1) {
+        exit(1);
+      }
+      array_[index]  = obj;
+      count_++;
+      if(index > lastElemIndex_) {
+        lastElemIndex_ = index;
+      }
+  }
+
+  Object* get(size_t index){
+    return array_[index];
+  }
+
+  // remove the item at the given index
+  Object* remove(size_t index) {
+    if(index > len_ -1) {
+      exit(1);
+    }
+    Object* removed = get(index);
+    array_[index] = nullptr;
+    count_--;
+
+    if(index == lastElemIndex_) {
+      lastElemIndex_= getLastIndex();
+    }
+    return removed;
+  }
+
+  void grow(){
+    Object** temp_data = new Object* [len_ * 2];
+    for(size_t i = 0; i < len_; i++) {
+      temp_data[i] = array_[i];
+    }
+    array_ = temp_data;
+    len_ *= 2;
+  }
+
+  size_t getLastIndex() {
+    for(int i = len_ - 1; i >= 0; i--) {
+      if(!array_[i]) {
+        return i;
+      }
+    }
+  }
+
+  // append given object to the array if there is space
+  // doubles the size of the array if an item occupies the length-1 index
+  void append(Object* to_add) {
+    if(count_ + 1 >= len_) {
+      grow();
+    }
+    array_[lastElemIndex_ + 1] = to_add;
+    lastElemIndex_++;
+    count_++;
+  }
+};
+
 
 class IntArray : public Array {
 public:
-	int** array;
+	int** array_;
 
-	IntArray(size_t len){
-		len_ = len;
-		count_ = 0;
-		array = new int[len_];
+	IntArray(size_t len) : Array(len){
+		array_ = new int* [len_];
 	}
 
 	// inserts the given object into the array and returns the array
-  	void insert(void* obj, size_t index) {
-  		array[index] = (int) obj;
+  	void insert(int* obj, size_t index) {
+      if(index > len_ - 1) {
+        exit(1);
+      }
+      array_[index]  = obj;
+      count_++;
+      if(index > lastElemIndex_) {
+        lastElemIndex_ = index;
+      }
   	}
 
   	// remove the item at the given index
-  	int* remove(size_t index) {
+  	int remove(size_t index) {
   		if (index > (len_ - 1)){
   			exit(1);
   		}
-  		int* removed = get(index);
-  		array[index] = NULL;
-  		count_ = count_ - 1;
+  		int removed = get(index);
+  		array_[index] = nullptr;
+  		count_ -= 1;
   		return removed;
   	}
 
   	// gets the object from data stored at index
-  	int* get(size_t index){
-  		return array[index];
+  	int get(size_t index){
+      int* elem = array_[index];
+      if(elem) {
+  		    return *array_[index];
+      } else {
+        exit(1);
+      }
   	}
+
+    void push_back(int* obj){
+      if(count_ + 1 >= len_) {
+        grow();
+      }
+      array_[lastElemIndex_ + 1] = obj;
+      lastElemIndex_++;
+    }
+
+    void grow(){
+      int** temp_data = new int* [len_ * 2];
+      for(size_t i = 0; i < len_; i++) {
+        temp_data[i] = array_[i];
+      }
+      array_ = temp_data;
+      len_ *= 2;
+    }
+
+    // append given object to the array if there is space
+    // doubles the size of the array if an item occupies the length-1 index
+    void append(int* to_add) {
+      if(count_ + 1 >= len_) {
+        grow();
+      }
+      array_[lastElemIndex_ + 1] = to_add;
+      lastElemIndex_++;
+      count_++;
+    }
 };
 
 class BoolArray : public Array {
 public:
-	bool** array;
+	bool** array_;
 
-	BoolArray(size_t len){
-		len_ = len;
-		count_ = 0;
-		array = new bool[len_];
+	BoolArray(size_t len) : Array(len){
+		array_ = new bool* [len_];
 	}
 
 	// inserts the given object into the array and returns the array
-  	void insert(void* obj, size_t index) {
-  		array[index] = (bool) obj;
+  	void insert(bool* obj, size_t index) {
+      if(index > len_ - 1) {
+        exit(1);
+      }
+      array_[index]  = obj;
+      count_++;
+      if(index > lastElemIndex_) {
+        lastElemIndex_ = index;
+      }
   	}
 
   	// remove the item at the given index
-  	bool* remove(size_t index) {
+  	bool remove(size_t index) {
   		if (index > (len_ - 1)){
   			exit(1);
   		}
-  		int* removed = get(index);
-  		array[index] = NULL;
+  		bool removed = get(index);
+  		array_[index] = NULL;
   		count_ = count_ - 1;
   		return removed;
   	}
 
   	// gets the object from data stored at index
-  	bool* get(size_t index){
-  		return array[index];
+  	bool get(size_t index){
+  		return *array_[index];
   	}
 
+    void grow(){
+      bool** temp_data = new bool* [len_ * 2];
+      for(size_t i = 0; i < len_; i++) {
+        temp_data[i] = array_[i];
+      }
+      array_ = temp_data;
+      len_ *= 2;
+    }
+
+    // append given object to the array if there is space
+    // doubles the size of the array if an item occupies the length-1 index
+    void append(bool* to_add) {
+      if(count_ + 1 >= len_) {
+        grow();
+      }
+      array_[lastElemIndex_ + 1] = to_add;
+      lastElemIndex_++;
+      count_++;
+    }
 };
 
 class FloatArray : public Array {
 public:
+  float** array_;
 
-	FloatArray(size_t len){
-		len_ = len;
-		count_ = 0;
-		array = new float[len_];
+	FloatArray(size_t len) : Array(len){
+		array_ = new float* [len_];
 	}
 
 	// inserts the given object into the array and returns the array
-  	void insert(void* obj, size_t index) {
-  		array[index] = (float) obj;
+  	void insert(float* obj, size_t index) {
+      if(index > len_ - 1) {
+        exit(1);
+      }
+      array_[index]  = obj;
+      count_++;
+      if(index > lastElemIndex_) {
+        lastElemIndex_ = index;
+      }
   	}
 
   	// remove the item at the given index
-  	float* remove(size_t index) {
+  	float remove(size_t index) {
   		if (index > (len_ - 1)){
   			exit(1);
   		}
-  		int* removed = get(index);
-  		array[index] = NULL;
+  		float removed = get(index);
+  		array_[index] = nullptr;
   		count_ = count_ - 1;
   		return removed;
   	}
 
   	// gets the object from data stored at index
-  	float* get(size_t index){
-  		return array[index];
+  	float get(size_t index){
+  		return *array_[index];
   	}
+
+    void grow(){
+      float** temp_data = new float* [len_ * 2];
+      for(size_t i = 0; i < len_; i++) {
+        temp_data[i] = array_[i];
+      }
+      array_ = temp_data;
+      len_ *= 2;
+    }
+
+    // append given object to the array if there is space
+    // doubles the size of the array if an item occupies the length-1 index
+    void append(float* to_add) {
+      if(count_ + 1 >= len_) {
+        grow();
+      }
+      array_[lastElemIndex_ + 1] = to_add;
+      lastElemIndex_++;
+      count_++;
+    }
 };
+
 
 class StringArray : public Array {
 public:
+  String** array_;
 
-	StringArray(size_t len){
-		len_ = len;
-		count_ = 0;
-	}
+  // Constructor
+  StringArray(size_t len) : Array(len) {
+    array_ = new String* [len];
+  }
+
+  void insert(String* obj, size_t index) {
+      if(index > len_ - 1) {
+        exit(1);
+      }
+      array_[index]  = obj;
+      count_++;
+      if(index > lastElemIndex_) {
+        lastElemIndex_ = index;
+      }
+  }
+
+  String* get(size_t index){
+    return array_[index];
+  }
+
+  // remove the item at the given index
+  String* remove(size_t index) {
+    if(index > len_ -1) {
+      exit(1);
+    }
+    String* removed = get(index);
+    array_[index] = nullptr;
+    count_--;
+
+    if(index == lastElemIndex_) {
+      lastElemIndex_= getLastIndex();
+    }
+    return removed;
+  }
+
+  void grow(){
+    String** temp_data = new String* [len_ * 2];
+    for(size_t i = 0; i < len_; i++) {
+      temp_data[i] = array_[i];
+    }
+    array_ = temp_data;
+    len_ *= 2;
+  }
+
+  size_t getLastIndex() {
+    for(int i = len_ - 1; i >= 0; i--) {
+      if(!array_[i]) {
+        return i;
+      }
+    }
+  }
+
+  // append given object to the array if there is space
+  // doubles the size of the array if an item occupies the length-1 index
+  void append(String* to_add) {
+    if(count_ + 1 >= len_) {
+      grow();
+    }
+    array_[lastElemIndex_ + 1] = to_add;
+    lastElemIndex_++;
+    count_++;
+  }
 };
-
-
-
-
-
