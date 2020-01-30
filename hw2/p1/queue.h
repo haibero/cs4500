df682889af9165e622cbb4a5cafb4cfc87243784
NickSimmons97 <nicholas.simmons100@gmail.com>
2020-01-30 18:12:54 -0500

    implemented queue

--- hw2/p1/queue.h
+++ hw2/p1/queue.h
@@ -1,38 +1,58 @@
+#pragma once
 #include "object.h"
+#include "string.h"
+#include "array.h"
 
 class Queue : public Object {
 public:
-  
-  // list of Objects
-  Object** q;
-  
+  size_t FIRST = 0;
+  ObjArray* q;
+
+
+  Queue(){
+    q = new ObjArray(8);
+  }
   
   /**
    *  adds an object to the end of this queue
    */
   void enqueue(Object* o) {
-    
+    q->append(o);
   }
   
   /**
    *  Pops the first object in this queue
    */
   Object* dequeue() {
-    return nullptr;
+    Object* removed = q->remove(FIRST);
+    ObjArray* new_q = new ObjArray(q->len_);
+    for (int i = 0; i < q->count(); ++i)
+    {
+      new_q->insert(q->get(i+1), i);
+    }
+    ObjArray* old_q = q;
+    q = new_q;
+    delete old_q;
+
+    return removed;
   }
   
   /**
    * Returns the first object in this queue without removing.
    */
   Object* peek() {
-    return nullptr;
+    if (size() < 1){
+      exit(1);
+    }
+
+    return q->get(FIRST);
   }
   
   /**
    * Returns the size of this queue.
    */
   size_t size() {
-    return 0;
+    return q->count();
   }
   
   size_t hash() {
@@ -43,8 +63,5 @@ public:
     return false;
   }
   
-  const char* to_string() {
-    return "";
-  }
-  
+
 };
