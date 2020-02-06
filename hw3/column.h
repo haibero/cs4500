b356da5189d042a0388f1d85653132d56f8085a7
NickSimmons97 <nicholas.simmons100@gmail.com>
2020-02-06 16:29:02 -0500

    added methods

--- hw3/column.h
+++ hw3/column.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdarg.h>
 #include "object.h"
 #include "string.h"
 
@@ -8,34 +9,98 @@ public:
 	Column();
 	~Column();
 
+	// determines if a column has a name
+	// True if String* type exists, False if null
+	bool columnNameExist();
+
+	// gets column's String* type name
+	// should check existence with 'columnNameExist()' first 
+	String* getColumnName();
+
+	// setter for a column's name
+	void setColumnName(String* name);
+
 };
 
 class StringColumn : public Column
 {
 public:
-	StringColumn(arguments);
+	StringColumn(String* cell, ...);
 	~StringColumn();
 
-	/* data */
+	// gets String* cell value at row number in column
+	String* get(int row);
+
+	// prints this column of Strings (default will not print name)
+	// header will be included if 'true' is passed as arguement
+	void printColumn(bool withName = false);
+
+	// determines if the input object is also a StringColumn
+	bool typeEqual(Object* column);
+
+	// determines if an input object is equal to this StringColumn
+	bool equal(Object* other);
 };
 
 class BoolColumn : public Column
 {
 public:
-	BoolColumn(arguments);
+	BoolColumn(bool cell, ...);
 	~BoolColumn();
 
-	/* data */
+	// gets bool cell value at row number in column
+	bool get(int row);
+
+	// prints this column of bools (default will not print name)
+	// header will be included if 'true' is passed as arguement
+	void printColumn(bool withName = false);
+	
+	// determines if the input object is also a BoolColumn
+	bool typeEqual(Object* column);
+
+	// determines if an input object is equal to this BoolColumn
+	bool equal(Object* other);
 };
 
 
 class IntColumn : public Column
 {
 public:
-	IntColumn(arguments);
+	IntColumn(int cell, ...);
 	~IntColumn();
 
-	/* data */
+	// gets int cell value at row number in column
+	int get(int row);
+
+	// prints this column of ints (default will not print name)
+	// header will be included if 'true' is passed as arguement
+	void printColumn(bool withName = false);
+	
+	// determines if the input object is also a IntColumn
+	bool typeEqual(Object* column);
+
+	// determines if an input object is equal to this IntColumn
+	bool equal(Object* other);
+};
+
+class FloatColumn : public Column
+{
+public:
+	FloatColumn(int cell, ...);
+	~FloatColumn();
+
+	// gets float cell value at row number in column
+	float get(int row);
+
+	// prints this column of floats (default will not print name)
+	// header will be included if 'true' is passed as arguement 
+	void printColumn(bool withName = false);
+	
+	// determines if the input object is also a FloatColumn
+	bool typeEqual(Object* column);
+
+	// determines if an input object is equal to this FloatColumn
+	bool equal(Object* other);
 };
 
 
--- hw3/dataframe.h
+++ hw3/dataframe.h
@@ -1,14 +1,66 @@
 #pragma once
+#include <stdarg.h>
 #include "object.h"
+#include "column.h"
+
 
 
 class Dataframe : public Object
 {
 public:
-	Dataframe(arguments);
+	Dataframe(Column* column, ...);
 	~Dataframe();
 
-	/* data */
+	// prints this dataframe
+	// this includes column names (or numbers), row names (if present)
+	// and all column values
+	void print();
+
+	// returns the number of rows in this dataframe
+	int nrow();
+
+	// returns the number of columns in this dataframe
+	int ncol();
+
+	// determines if each column has a name
+	// true if every column has a name, otherwise false
+	bool columnNamesExist();
+
+	// returns array of all column names
+	// * all columns must have names, should check with 'columnNamesExist()' first
+	String** getColumnNames();
+
+	// determines if each row has a name
+	// true if every row has a name, otherwise false
+	bool rowNamesExist();
+
+	// returns array of all row names
+	// * all rows must have names, should check with 'rowNamesExist()' first
+	String** getRowNames();
+
+	// column-wise combines df2 to left horizontal end of this dataframe 
+	// * both dataframes must have the same number of rows
+	void cbind(Dataframe* df2);
+
+	// row-wise combinds df2 to the bottom of this dataframe 
+	// * both dataframes must have the same number of columns
+	// * each corresponding column number must be of the same type
+	void rbind(Dataframe* df2);
+
+	// gets column in dataframe by name
+	Column* getColByName(String* columnName);
+
+	// gets column in dataframe by column number 
+	// * possible indices: 0 -> (ncol() - 1)
+	Column* getColByNum(int columnIndx);
+
+	// gets row (array of 1 element column objects) in dataframe by row name
+	Column** getRowByName(String* rowName);
+
+	// gets row (array of 1 element column objects) in dataframe by row number 
+	// * possible indices: 0 -> (rcol() - 1)
+	Column** getRowByNum(int rowIndx);
+
 };
 
 
